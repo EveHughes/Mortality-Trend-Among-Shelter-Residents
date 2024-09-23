06dfe9a810ddba2577a99e4e99e9d2fd2edba947
Mandy He <h.mandy.777@gmail.com>
2024-09-23 10:24:48 -0400

    Initial commit

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Simulates... [...UPDATE THIS...]
+# Author: Mandy He
+# Date: 23 September 2024 
+# Contact: mandyxy.he@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# Any other information needed? 
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
---
+++ scripts/01-download_data.R
@@ -0,0 +1,29 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Author: Mandy He
+# Date: 23 September 2024 
+# Contact: mandyxy.he@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+library(dplyr)
+
+
+#### Download data ####
+
+package <- show_package("91cf5765-3d3f-4d55-9eea-acb1d4222928")
+package
+
+
+
+#### Save data ####
+# [...UPDATE THIS...]
+# change the_raw_data to whatever name you assigned when you downloaded it.
+write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+         
